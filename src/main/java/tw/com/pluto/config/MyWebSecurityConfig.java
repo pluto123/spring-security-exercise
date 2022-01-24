0953449cb616eadd5c7bb44b62d6c6d58eda9968
Steven Wu <stevenwu@mirle.com.tw>
2022-01-24 11:55:27 +0800

    設定階層權限

--- src/main/java/tw/com/pluto/config/MyWebSecurityConfig.java
+++ src/main/java/tw/com/pluto/config/MyWebSecurityConfig.java
@@ -1,7 +1,10 @@
 package tw.com.pluto.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -21,10 +24,18 @@ public class MyWebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .formLogin().and()
-                .authorizeHttpRequests()
+                .authorizeRequests()
                 .mvcMatchers("/").permitAll()
                 .mvcMatchers("/user/**").hasRole("USER")
                 .mvcMatchers("/admin/**").hasRole("ADMIN")
                 .anyRequest().authenticated();
+    }
+
+    @Bean
+    RoleHierarchy hierarchy() {
+        RoleHierarchyImpl hierarchy = new RoleHierarchyImpl();
+        hierarchy.setHierarchy("ROLE_ADMIN > ROLE_USER");
+
+        return hierarchy;
     }
 }
