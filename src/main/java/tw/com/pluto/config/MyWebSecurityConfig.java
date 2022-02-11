53395d7ddbb11b44d9a951853cad8ad1696a54e6
Steven Wu <stevenwu@mirle.com.tw>
2022-02-11 17:15:55 +0800

    add comment

--- src/main/java/tw/com/pluto/config/MyWebSecurityConfig.java
+++ src/main/java/tw/com/pluto/config/MyWebSecurityConfig.java
@@ -15,7 +15,6 @@ import tw.com.pluto.security.handler.MyAccessDeniedHandler;
 import tw.com.pluto.security.handler.MyAuthenticationFailureHandler;
 import tw.com.pluto.security.handler.MyAuthenticationSuccessHandler;
 import tw.com.pluto.security.handler.MyLogoutSuccessHandler;
-import tw.com.pluto.security.strategy.MySessionInformationExpiredStrategy;
 
 @Configuration
 public class MyWebSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -31,8 +30,6 @@ public class MyWebSecurityConfig extends WebSecurityConfigurerAdapter {
     MyAccessDeniedHandler myAccessDeniedHandler;
     @Autowired
     UnauthorizedEntryPoint unauthorizedEntryPoint;
-    @Autowired
-    MySessionInformationExpiredStrategy mySessionInformationExpiredStrategy;
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -41,25 +38,20 @@ public class MyWebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors().and().csrf().disable();
-
-        http
-                .authorizeRequests()
+        http.cors().and()  // 允許跨域
+                .csrf().disable()  // 關閉 CSRF
+                .authorizeRequests()  // 設定權限
                 .mvcMatchers("/user/**").hasRole("USER")
-                .mvcMatchers("/admin/**").hasRole("ADMIN")
-                .and().formLogin().permitAll()
-                .successHandler(myAuthenticationSuccessHandler) // 登录成功逻辑处理
-                .failureHandler(myAuthenticationFailureHandler) // 登录失败逻辑处理
-
-                .and()
-                .logout()   //开启注销
-                .permitAll()    //允许所有人访问
-                .logoutSuccessHandler(myLogoutSuccessHandler) //注销逻辑处理
-                .deleteCookies("JSESSIONID")    //删除cookie
-
-                .and().exceptionHandling()
-                .accessDeniedHandler(myAccessDeniedHandler)    //权限不足的时候的逻辑处理
-                .authenticationEntryPoint(unauthorizedEntryPoint);  //未登录是的逻辑处理
+                .mvcMatchers("/admin/**").hasRole("ADMIN").and()
+                .formLogin().permitAll()  // 允許每個使用者都可訪問登入 URL
+                .successHandler(myAuthenticationSuccessHandler) // 登入成功後的處理程序
+                .failureHandler(myAuthenticationFailureHandler).and() // 登入失敗後的處理程序
+                .logout().permitAll()  // 允許每個使用者都可訪問登出 URL
+                .logoutSuccessHandler(myLogoutSuccessHandler)  // 登出成功後的處理程序
+                .deleteCookies("JSESSIONID").and()  // 刪除 SESSION Cookie
+                .exceptionHandling()
+                .accessDeniedHandler(myAccessDeniedHandler)  // 無權限訪問的處理程序
+                .authenticationEntryPoint(unauthorizedEntryPoint); // 未驗證時的處理程序
     }
 
     @Bean
--- src/main/java/tw/com/pluto/security/entry/UnauthorizedEntryPoint.java
+++ src/main/java/tw/com/pluto/security/entry/UnauthorizedEntryPoint.java
@@ -16,16 +16,16 @@ import java.nio.charset.StandardCharsets;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
+// 未經過驗證又訪問 URL 時，返回訊息給前端的
 @Component
 public class UnauthorizedEntryPoint implements AuthenticationEntryPoint {
     @Autowired
     ObjectMapper objectMapper;
 
-    // 未登入時返回前端的數據
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException e) throws IOException, ServletException {
         Map<String, String> message = new LinkedHashMap<>();
-        message.put("message", "需要登入!!");
+        message.put("message", "Login Required");
 
         response.setStatus(HttpStatus.UNAUTHORIZED.value());
         response.setContentType(MediaType.APPLICATION_JSON_VALUE);
